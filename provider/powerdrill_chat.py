99fd2b1dd35a640808c350cf6c0d367f44968a22
qingye <qingye@zbyte-inc.com>
2025-03-07 15:19:16 +0800

    first commit with empty emplate

---
+++ provider/powerdrill_chat.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+from dify_plugin import ToolProvider
+from dify_plugin.errors.tool import ToolProviderCredentialValidationError
+
+
+class PowerdrillChatProvider(ToolProvider):
+    def _validate_credentials(self, credentials: dict[str, Any]) -> None:
+        pass
